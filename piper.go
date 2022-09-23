d99bd3ee097c5985d0cdf1addc77ae5f0dc9fdec
Alexandru Eftimie - mac <eftimie.alexandru@gmail.com>
2022-09-23 20:32:56 +0300

    Now using thread safe checks, using go copy code

--- piper.go
+++ piper.go
@@ -1,13 +1,22 @@
-package netplus // import go.ideatocode.tech/netplus
+package netplus
 
 import (
 	"context"
+	"errors"
 	"io"
+	"sync/atomic"
 
 	"time"
 
 	"go.ideatocode.tech/log"
 )
+
+// ErrShortWrite means that a write accepted fewer bytes than requested
+// but failed to return an explicit error.
+var ErrShortWrite = errors.New("short write")
+
+// errInvalidWrite means that a write returned an impossible count.
+var errInvalidWrite = errors.New("invalid write result")
 
 // Piper .
 type Piper struct {
@@ -25,13 +34,13 @@ func NewPiper(l log.Logger, t time.Duration) *Piper {
 }
 
 // Debug turns debugging on and off
-func (p Piper) Debug(debug bool) {
+func (p *Piper) Debug(debug bool) {
 	p.debug = debug
 }
 
 // Run pipes data between upstream and downstream and closes one when the other closes
 // times out after two hours by default
-func (p Piper) Run(ctx context.Context, downstream io.ReadWriteCloser, upstream io.ReadWriteCloser) {
+func (p *Piper) Run(ctx context.Context, downstream io.ReadWriteCloser, upstream io.ReadWriteCloser) (written int64, err error) {
 	var dur time.Duration
 	if p.Timeout == 0 {
 		dur = time.Duration(2 * time.Hour)
@@ -39,82 +48,88 @@ func (p Piper) Run(ctx context.Context, downstream io.ReadWriteCloser, upstream 
 		dur = p.Timeout
 	}
 
-	done := false
-	cancel := func() {
-		if done {
-			return
-		}
-		done = true
-		ctx.Done()
-		downstream.Close()
-		upstream.Close()
-		if p.debug {
-			p.Logger.Debug("Closing sockets")
-		}
-	}
-	p.idleTimeoutPipe(ctx, downstream, upstream, dur, cancel)
+	return p.idleTimeoutPipe(ctx, downstream, upstream, dur)
 }
 
-func (p Piper) idleTimeoutPipe(ctx context.Context, dst io.ReadWriter, src io.ReadWriter, timeout time.Duration,
-	cancel context.CancelFunc) (written int64, err error) {
-	read := make(chan int)
+func (p *Piper) idleTimeoutPipe(ctx context.Context, dst io.ReadWriteCloser, src io.ReadWriteCloser, timeout time.Duration) (written int64, err error) {
+
+	var done int32 = 0
+
+	ctx, closeContext := context.WithCancel(ctx)
+
+	closeBothSockets := func() {
+		if atomic.CompareAndSwapInt32(&done, 1, 1) {
+			return
+		}
+		closeContext()
+		src.Close()
+		dst.Close()
+	}
+	timekeeper := make(chan struct{})
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			case <-time.After(timeout):
-
-				p.Logger.Debug("idleTimeoutPipe timeout reached")
-				cancel()
+				if p.debug {
+					p.Logger.Debug("idletimeoutpipe: timeout reached")
+				}
+				closeBothSockets()
 				return
-			case <-read:
+			case <-timekeeper:
 			}
 		}
 	}()
-
+	var w1, w2 int64
+	var err1, err2 error
+	var ec = make(chan error, 2)
 	go func() {
-		defer cancel()
-
-		buf := make([]byte, 32*1024)
-		for {
-			nr, er := dst.Read(buf)
-			if nr > 0 {
-				read <- nr
-				nw, ew := src.Write(buf[0:nr])
-				written += int64(nw)
-				if ew != nil {
-					err = ew
-					break
-				}
-				if p.debug && nw != nr {
-					p.Logger.Error("Broken Write")
-				}
-			}
-			if er != nil {
-				if er != io.EOF {
-					err = er
-				}
-				break
-			}
-		}
+		w1, err1 = copy(src, dst, timekeeper)
+		ec <- err1
 	}()
+	go func() {
+		w2, err2 = copy(dst, src, timekeeper)
+		ec <- err2
+	}()
+	firstErr := <-ec
+	closeBothSockets()
+	<-ec // empty the channel, equivallent to wg.Wait
 
-	defer cancel()
-	buf := make([]byte, 32*1024)
+	return w1 + w2, firstErr
+}
+
+func copy(src io.Reader, dst io.Writer, timekeeper chan struct{}) (written int64, err error) {
+
+	size := 32 * 1024
+	if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+		if l.N < 1 {
+			size = 1
+		} else {
+			size = int(l.N)
+		}
+	}
+	buf := make([]byte, size)
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
-			read <- nr
 			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = errInvalidWrite
+				}
+			}
 			written += int64(nw)
 			if ew != nil {
 				err = ew
 				break
 			}
-			if p.debug && nw != nr {
-				p.Logger.Error("Broken Write")
+			if nr != nw {
+				err = ErrShortWrite
+				break
 			}
+			timekeeper <- struct{}{}
 		}
 		if er != nil {
 			if er != io.EOF {
